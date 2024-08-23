463c880cd8e53c1f4af12c003e4eabeaa51075d9
Vinay0726 <vinaythaware143@gmail.com>
2024-08-23 18:17:07 +0530

    project start

---
+++ src/main/java/com/tasknext/tasknext/TaskNestApplication.java
@@ -0,0 +1,13 @@
+package com.tasknext.tasknext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasknext/tasknext/TaskNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasknext.tasknext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
